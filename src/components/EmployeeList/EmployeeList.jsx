a32d0b7ebf0bad4717dbdd609c3afed0d888feaa
Mariam J. Hussein <engmhussein86@gmail.com>
2024-05-29 15:45:42 -0400

    update

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,12 +1,13 @@
 import "./EmployeeList.css";
 import EmployeeListItem from "../EmployeeListItem/EmployeeListItem";
+import reactImg from '../../assets/react.svg';
 
 const employees=[
-    {name:'employee 1', title: 'title1'},
-    {name:'employee 2', title: 'title2'},
-    {name:'employee 3', title: 'title3'},
-    {name:'employee 4', title: 'title4'},
-    {name:'employee 5', title: 'title5'}
+    {name:'employee 1', title: 'title1', img: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Personal_photo.png'},
+    {name:'employee 2', title: 'title2', img: 'https://sb.kaleidousercontent.com/67418/1920x1282/9d56e09251/christina-wocintechchat-com-50tkcap8m3a-unsplash.jpg'},
+    {name:'employee 3', title: 'title3', img: 'https://sb.kaleidousercontent.com/67418/1920x1282/9d56e09251/christina-wocintechchat-com-50tkcap8m3a-unsplash.jpg'},
+    {name:'employee 4', title: 'title4', img: 'https://www.befunky.com/images/wp/wp-2021-01-Blog-Post-Header-1.jpg?auto=avif,webp&format=jpg&width=1150&crop=16:9'},
+    {name:'employee 5', title: 'title5', img: 'https://www.befunky.com/images/wp/wp-2021-01-Blog-Post-Header-1.jpg?auto=avif,webp&format=jpg&width=1150&crop=16:9'}
 ]
 ;
 
--- src/components/EmployeeListItem/EmployeeListItem.jsx
+++ src/components/EmployeeListItem/EmployeeListItem.jsx
@@ -6,8 +6,13 @@ function EmployeeListItem(props) {
 
   return (
     <div className="employeeItem">
+
+        <img src={props.obj.img}/>
+
+        <div>
         <h3>{props.obj.name}</h3>
         <h5>{props.obj.title}</h5>
+        </div>
 
     </div>
     
